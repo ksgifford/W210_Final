711aa185755de6e7dee1ece4e5cfac2c8422d457
Kevin Gifford <ksgifford@gmail.com>
2019-04-13 12:45:40 -0700

    Mapping data

--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -214,7 +214,7 @@ def output():
     df_cats = df_ConsvNW[(df_ConsvNW['label']=='cougar')|(df_ConsvNW['label']=='bobcat')][['Long','Lat']]
     cats = df_cats.values.tolist()
 
-    payload = list(geojson,species_all, bears, elk_deer, coyotes, cats)
+    payload = [geojson, species_all, bears, elk_deer, coyotes, cats]
 
     return render_template('output.html', title='Results Download', data=payload)
 
