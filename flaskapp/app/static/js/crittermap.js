d162f574bf8e7f1880cc6e1f1fc953de793a7e05
Kevin Gifford <ksgifford@gmail.com>
2019-04-13 17:25:41 -0700

    JS heatmap

--- flaskapp/app/static/js/crittermap.js
+++ flaskapp/app/static/js/crittermap.js
@@ -17,7 +17,7 @@ function initializePage(){
     fillOpacity: 0.8
   };
 
-  map = L.map("map").setView([47.439260, --121.253771], 8);
+  map = L.map("map").setView([47.439260, -121.253771], 8);
   L.tileLayer(
     "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
     {
