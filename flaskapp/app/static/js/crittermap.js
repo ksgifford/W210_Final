38cf94c9e2f5d964978d4624053f9de679caa008
Kevin Gifford <ksgifford@gmail.com>
2019-04-16 20:37:17 -0700

    Map styling

--- flaskapp/app/static/js/crittermap.js
+++ flaskapp/app/static/js/crittermap.js
@@ -10,7 +10,7 @@ function onEachFeature(feature, layer) {
 function initializePage(){
   var geojsonMarkerOptions = {
     radius: 4,
-    fillColor: "PaleTurquoise",
+    fillColor: "Turquoise",
     color: "#000",
     weight: 1,
     opacity: 1,
